d6668b920ec08ea02a24f681c93f0171c5f637a7
Michael Pucher <michael.pucher.main@gmail.com>
2016-04-11 03:02:31 +0100

    Removing stuff from interpreter, starting with implementation of basic assembler

--- interpreter.cpp
+++ interpreter.cpp
@@ -7,6 +7,8 @@
 #define OP_SUB 1
 #define OP_MUL 2
 #define OP_HLT 3
+#define OP_LOA 4
+#define OP_STR 5
 
 #define REG_00 0
 #define REG_01 1
@@ -41,24 +43,16 @@ int main() {
 	};	
 
 	// Main dispatch loop
-	// TODO: DO NOT USE PROGRAM COUNTER FOR OPERANDS
-	// (OFF BY ONE)
 	DP();
 	while (1) {	
 	op_add:
 		reg[code[pc - 1][2]] += reg[code[pc - 1][1]];
-		std::cout << reg[REG_00] << " "
-			  << reg[REG_01] << "\n";
 		DP();
 	op_sub:
 		reg[code[pc - 1][2]] -= reg[code[pc - 1][1]];
-		std::cout << reg[REG_00] << " "
-			  << reg[REG_01] << "\n";
 		DP();
 	op_mul:
 		reg[code[pc - 1][2]] *= reg[code[pc - 1][1]];
-		std::cout << reg[REG_00] << " "
-			  << reg[REG_01] << "\n";
 		DP();
 	op_hlt:
 		std::cout << "Interpreter halted\n";

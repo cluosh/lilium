e96b907ff072900c54032265adbced423caf7d85
Michael Pucher <michael.pucher.main@gmail.com>
2017-02-07 02:07:08 +0100

    Adding instruction parsing for hlt, nop and ldi

--- rust/lilium-as/src/main.rs
+++ rust/lilium-as/src/main.rs
@@ -3,6 +3,28 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
 use std::env;
+use std::fmt;
+
+/// Instruction, all instructions are exactly 4 byte long
+struct Instruction {
+    opcode: u8,
+    a0: u8,
+    a1: u8,
+    a2: u8,
+}
+
+/// Print the (byte) representation of an instruction
+impl fmt::Display for Instruction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Return the hexadecimal representation of the instruction
+        write!(f,
+               "{0:02x}{1:02x}{2:02x}{3:02x}",
+               self.opcode,
+               self.a0,
+               self.a1,
+               self.a2)
+    }
+}
 
 /// Parse a single line of an assembly file:
 /// * Check whether a label has been defined.
@@ -26,7 +48,10 @@ fn parse_line(line: &str) {
         }
 
         // Handle instruction/annotation groups
-        process_instruction(token, iter);
+        match token {
+            x if x.ends_with('i') => process_intermediate(token, &mut iter),
+            _ => process_instruction(token, &mut iter),
+        }
     }
 }
 
@@ -35,7 +60,7 @@ fn parse_line(line: &str) {
 /// Arguments
 /// * **token**: the token corresponding to the label
 fn process_label(token: &str) {
-    println!("LABEL {}", token);
+    // TODO: Label processing
 }
 
 /// Process an instruction including its arguments, processed instructions
@@ -44,14 +69,71 @@ fn process_label(token: &str) {
 /// Arguments:
 /// * **instruction**: the token corresponding to the instruction name
 /// * **argument**: iterator for the rest of the instruction arguments
-fn process_instruction<'a, I>(instruction: &str, arguments: I)
+fn process_instruction<'a, I>(instruction: &str, arguments: &mut I)
     where I: Iterator<Item = &'a str>
 {
-    print!("INST {} ARGS ", instruction);
-    for arg in arguments {
-        print!("{} ", arg);
+    match instruction {
+        "hlt" => {
+            let inst = Instruction {
+                opcode: 0x00,
+                a0: 0x00,
+                a1: 0x00,
+                a2: 0x00,
+            };
+            println!("{}", inst);
+        }
+        "nop" => {
+            let inst = Instruction {
+                opcode: 0x01,
+                a0: 0x00,
+                a1: 0x00,
+                a2: 0x00,
+            };
+            println!("{}", inst);
+        }
+        _ => panic!("Invalid instruction: {}", instruction),
     }
-    println!();
+}
+
+/// Process an instruction using intermediate values, including its arguments,
+/// processed instructions are emitted to standard output.
+///
+/// Arguments:
+/// * **instruction**: the token corresponding to the instruction name
+/// * **argument**: iterator for the rest of the instruction arguments
+fn process_intermediate<'a, I>(instruction: &str, arguments: &mut I)
+    where I: Iterator<Item = &'a str>
+{
+    // Error messages
+    let missing_args = "Missing arguments for instruction";
+    let invalid_reg = "Invalid register specification";
+    let numeric_arg = "Expected numeric argument";
+
+    // Parse instructions
+    match instruction {
+        "ldi" => {
+            // Load register name
+            let reg_name = arguments.next().expect(missing_args);
+            let reg: String = reg_name.chars().skip(1).collect();
+            let reg: u8 = reg.parse().expect(invalid_reg);
+
+            // Load 16-bit number
+            let num = arguments.next().expect(missing_args);
+            let num: u16 = num.parse().expect(numeric_arg);
+            let lo = num as u8;
+            let hi = (num >> 8) as u8;
+
+            // Load register name and intermediate value
+            let inst = Instruction {
+                opcode: 0x02,
+                a0: reg,
+                a1: lo,
+                a2: hi,
+            };
+            println!("{}", inst);
+        }
+        _ => panic!("Invalid instruction: {}", instruction),
+    }
 }
 
 fn main() {
@@ -66,7 +148,7 @@ fn main() {
         };
 
         // Read assembler file line by line
-        let mut reader = BufReader::new(&file);
+        let reader = BufReader::new(&file);
         for line_result in reader.lines() {
             let line = match line_result {
                 Ok(line) => line,

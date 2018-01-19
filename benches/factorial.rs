4acf8bdb15bbbce91f58c1022b4503bffe5a2ccc
Michael Pucher <michael.pucher.main@gmail.com>
2018-01-19 19:33:09 +0100

    Adding sum benchmark

---
+++ benches/factorial.rs
@@ -0,0 +1,48 @@
+#![feature(test)]
+extern crate test;
+extern crate lilium;
+
+use test::Bencher;
+use lilium::*;
+
+#[bench]
+fn factorial(b: &mut Bencher) {
+    let Module {
+        functions: f,
+        constants: c,
+        entry_point: e,
+        code: i
+    } = compile("0");
+
+    let mut registers: [i64; 25600] = [0; 25600];
+    let mut thread = Thread {
+        functions: &f,
+        constants: &c,
+        code: &i,
+        registers: &mut registers,
+        base: 0
+    };
+
+    b.iter(|| { run(&mut thread, e as usize) });
+}
+
+#[bench]
+fn factorial_tail(b: &mut Bencher) {
+    let Module {
+        functions: f,
+        constants: c,
+        entry_point: e,
+        code: i
+    } = compile("0");
+
+    let mut registers: [i64; 25600] = [0; 25600];
+    let mut thread = Thread {
+        functions: &f,
+        constants: &c,
+        code: &i,
+        registers: &mut registers,
+        base: 0
+    };
+
+    b.iter(|| { run(&mut thread, e as usize) });
+}
---
+++ benches/sum.rs
@@ -0,0 +1,61 @@
+#![feature(test)]
+extern crate test;
+extern crate lilium;
+
+use test::Bencher;
+use lilium::*;
+
+#[bench]
+fn sum(b: &mut Bencher) {
+    let Module {
+        functions: f,
+        constants: c,
+        entry_point: e,
+        code: i
+    } = compile(concat!("(def sum (a)",
+        "  (if",
+        "    (> a 0)",
+        "    ((+ a (sum (- a 1))))",
+        "    (1)))",
+        "(sum 900)"
+        ));
+
+    let mut registers = vec![0; 256 * 902];
+    let mut thread = Thread {
+        functions: &f,
+        constants: &c,
+        code: &i,
+        registers: &mut registers,
+        base: 0
+    };
+
+    b.iter(|| { run(&mut thread, e as usize) });
+}
+
+#[bench]
+fn sum_tail(b: &mut Bencher) {
+    let Module {
+        functions: f,
+        constants: c,
+        entry_point: e,
+        code: i
+    } = compile(concat!(
+        "(def sum (a b)",
+        "  (if ",
+        "     (> a 0)",
+        "     ((sum (- a 1) (+ b 1)))",
+        "     ((+ b 1))))",
+        "(sum 900 0)"
+    ));
+
+    let mut registers: [i64; 1536] = [0; 1536];
+    let mut thread = Thread {
+        functions: &f,
+        constants: &c,
+        code: &i,
+        registers: &mut registers,
+        base: 0
+    };
+
+    b.iter(|| { run(&mut thread, e as usize) });
+}

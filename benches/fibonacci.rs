ef7259e1fc185101850216e48f966cfb953d41b3
Michael Pucher <michael.pucher.main@gmail.com>
2018-01-31 02:53:05 +0100

    Adding additional fibonacci benchmark

--- benches/fibonacci.rs
+++ benches/fibonacci.rs
@@ -60,3 +60,31 @@ fn fibonacci_tail(b: &mut Bencher) {
 
     b.iter(|| { run(&mut thread, e as usize) });
 }
+
+#[bench]
+fn fibonacci_big(b: &mut Bencher) {
+    let Module {
+        functions: f,
+        constants: c,
+        entry_point: e,
+        code: i
+    } = compile(concat!(
+    "(def fib (a b c)",
+    "  (if",
+    "  (> c 1)",
+    "  ((fib b (+ a b) (- c 1)))",
+    "  (b)))",
+    "(fib 0 1 1000000)"
+    ));
+
+    let mut registers: [i64; 1024] = [0; 1024];
+    let mut thread = Thread {
+        functions: &f,
+        constants: &c,
+        code: &i,
+        registers: &mut registers,
+        base: 0
+    };
+
+    b.iter(|| { run(&mut thread, e as usize) });
+}
